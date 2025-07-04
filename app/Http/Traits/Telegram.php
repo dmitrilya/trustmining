627f03bf5ff50297a03aaccf16636dfecfb4b0db
dmitrilya <dmitrievilyan@gmail.com>
2025-07-04 11:50:18 +0800

    tg auth fix

--- app/Http/Traits/Telegram.php
+++ app/Http/Traits/Telegram.php
@@ -23,7 +23,7 @@ trait Telegram
 
         sort($data_check_arr);
         $data_check_string = implode("\n", $data_check_arr);
-        $secret_key = hash('sha256', env('NOTIFICATION_BOT_TOKEN'), true);
+        $secret_key = hash('sha256', config('services.tgbot.token'), true);
         $hash = hash_hmac('sha256', $data_check_string, $secret_key);
 
         if (strcmp($hash, $check_hash) !== 0 || (time() - $data['auth_date']) > 86400)
