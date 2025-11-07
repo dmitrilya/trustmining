2ec2f83ccaa35def43da84a47bca64958a73bc7a
dmitrilya <dmitrievilyan@gmail.com>
2025-11-07 12:54:11 +0800

    amo auth

--- app/Services/CRM/AmoCRMService.php
+++ app/Services/CRM/AmoCRMService.php
@@ -235,7 +235,7 @@ class AmoCRMService extends BaseCRMService
         $contentType = 'application/json';
         $headers = [
             'Content-Type' => $contentType,
-            'Authorization: Bearer' => $accessToken
+            'Authorization' => "Bearer $accessToken"
         ];
 
         $request = Http::withHeaders($headers);
