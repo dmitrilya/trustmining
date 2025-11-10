c2afd4011d24b4117ae9dee96ef3b7a1cf2306ce
dmitrilya <dmitrievilyan@gmail.com>
2025-11-10 13:33:26 +0800

    hashrate need blocks time calculation

--- app/Http/Controllers/MetricsController.php
+++ app/Http/Controllers/MetricsController.php
@@ -48,6 +48,19 @@ class MetricsController extends Controller
             foreach ($difficulties as $i => $difficulty) {
                 if (!isset($difficulties[$i + 1])) return back();
 
+                if (!$needBlocksTime && $i == 6) {
+                    $time = ($lastDifficulty->created_at - $difficulty->created_at) / ($difficulty->need_blocks - $lastDifficulty->need_blocks) * $lastDifficulty->need_blocks;
+                    $days = intdiv($time, 60 * 60 * 24);
+                    $time %= (60 * 60 * 24);
+                    $hours = intdiv($time, 60 * 60);
+                    $time %= (60 * 60);
+                    $minutes = intdiv($time, 60);
+                    $needBlocksTime = '~';
+                    if ($days > 0) $needBlocksTime .= $days . ' ' . trans_choice('time.days', $days) . ' ';
+                    if ($hours > 0) $needBlocksTime .= $hours . ' ' . trans_choice('time.hours', $hours) . ' ';
+                    if ($minutes > 0) $needBlocksTime .= $minutes . ' ' . trans_choice('time.minutes', $minutes);
+                }
+
                 if ($difficulty->need_blocks > $difficulties[$i + 1]->need_blocks) {
                     if (!$needBlocksTime) {
                         if ($i == 0) $needBlocksTime = __('Time calculation');
