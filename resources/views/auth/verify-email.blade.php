c89bf4780ebbdfb834a607866ed2581b30a5f17b
dmitrilya <dmitrievilyan@gmail.com>
2025-12-20 13:08:33 +0800

    verify email text fix

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-700 dark:text-gray-300">
-        {{ __("Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.") }}
+        {{ __("Before continuing, you'll need to confirm your email address by clicking the link we just sent you. If you haven't received the email, we'll be happy to send you another one.") }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
