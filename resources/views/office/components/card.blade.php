86b3e6800fb7da764284eb785d4e845373c285c3
dmitrilya <dmitrievilyan@gmail.com>
2025-12-21 23:34:10 +0800

    office card view fix

--- resources/views/office/components/card.blade.php
+++ resources/views/office/components/card.blade.php
@@ -1,5 +1,5 @@
 <li class="h-full">
-    <p class="hidden sm:flex items-center text-sm sm:text-base font-semibold text-gray-800 dark:text-gray-200 mb-6">
+    <p class="hidden sm:flex items-center text-sm font-semibold text-gray-800 dark:text-gray-200 mb-6">
         <svg class="min-w-4 w-4 h-4 sm:min-w-6 sm:w-6 sm:h-6 text-gray-600 mr-2" aria-hidden="true" width="24"
             height="24" fill="currentColor" viewBox="0 0 24 24">
             <path fill-rule="evenodd"
@@ -9,13 +9,13 @@
         {{ $office->address }}
     </p>
 
-    <div class="flex flex-col justify-between sm:grid grid-cols-2 gap-3 sm:gap-6 xl:gap-4">
+    <div class="h-full flex flex-col justify-between sm:grid grid-cols-2 gap-3 sm:gap-6 xl:gap-4">
         <x-carousel :images="$office->images"></x-carousel>
 
         <div class="flex flex-col justify-between w-full">
             <div>
                 <p
-                    class="flex sm:hidden items-center text-sm sm:text-base font-semibold text-gray-800 dark:text-gray-200 mb-3">
+                    class="flex sm:hidden items-center text-xs font-semibold text-gray-800 dark:text-gray-200 mb-3">
                     <svg class="min-w-4 w-4 h-4 sm:min-w-6 sm:w-6 sm:h-6 text-gray-600 mr-2" aria-hidden="true"
                         width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd"
