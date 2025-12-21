58f60298dd47d0f757a4d3cb0e6665e0e8fb55bd
dmitrilya <dmitrievilyan@gmail.com>
2025-12-21 23:26:04 +0800

    office card view fix

--- resources/views/office/components/card.blade.php
+++ resources/views/office/components/card.blade.php
@@ -1,4 +1,4 @@
-<li>
+<li class="h-full">
     <p class="hidden sm:flex items-center text-sm sm:text-base font-semibold text-gray-800 dark:text-gray-200 mb-6">
         <svg class="min-w-4 w-4 h-4 sm:min-w-6 sm:w-6 sm:h-6 text-gray-600 mr-2" aria-hidden="true" width="24"
             height="24" fill="currentColor" viewBox="0 0 24 24">
