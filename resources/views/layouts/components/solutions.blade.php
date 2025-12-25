5c917d83b296d6d97b8e1e7b1bf1ba5dc6bd8b01
dmitrilya <dmitrievilyan@gmail.com>
2025-12-25 22:07:22 +0800

    top menu

--- resources/views/layouts/components/solutions.blade.php
+++ resources/views/layouts/components/solutions.blade.php
@@ -62,7 +62,8 @@
                         <svg class="size-4 lg:size-5 text-gray-500 mr-2" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                             viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="1.5"
                                 d="M16 10.5h.01m-4.01 0h.01M8 10.5h.01M5 5h14a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-6.6a1 1 0 0 0-.69.275l-2.866 2.723A.5.5 0 0 1 8 18.635V17a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z" />
                         </svg>
                         <a class="under text-sm lg:text-base text-gray-900 dark:text-gray-200 font-semibold"
@@ -78,6 +79,18 @@
                         </svg>
                         <a class="under text-sm lg:text-base text-gray-900 dark:text-gray-200 font-semibold"
                             href="{{ route('companies') }}">{{ __('Companies') }}</a>
+                    </div>
+
+                    <div class="group flex items-center">
+                        <svg class="size-4 lg:size-5 text-gray-500 mr-2" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                            viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z" />
+                        </svg>
+
+                        <a class="under text-sm lg:text-base text-gray-900 dark:text-gray-200 font-semibold"
+                            href="{{ route('top') }}">{{ __('Top reliable companies') }}</a>
                     </div>
 
                     {{-- <div class="group flex items-center">
