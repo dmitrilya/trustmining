879d9397f3d893fed3da8ff70ce13a66559f1a8b
dmitrilya <dmitrievilyan@gmail.com>
2026-01-16 18:05:55 +0800

    shop menu fix

--- resources/views/shop/components/menu.blade.php
+++ resources/views/shop/components/menu.blade.php
@@ -11,7 +11,7 @@
         </div>
     </button>
 
-    <div x-show="open" class="absolute z-10 mt-5 flex max-w-md" style="width: calc(100vw - 2rem);display: none"
+    <div x-show="open" class="absolute z-20 mt-5 flex max-w-md" style="width: calc(100vw - 2rem);display: none"
         x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 translate-y-1"
         x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in duration-150"
         x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-1"
