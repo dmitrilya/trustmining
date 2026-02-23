e08162fb84421a75334cde8438e66a46c8ac9370
dmitrilya <dmitrievilyan@gmail.com>
2026-02-23 10:43:58 +0800

    fix

--- resources/views/components/length-textarea.blade.php
+++ resources/views/components/length-textarea.blade.php
@@ -3,7 +3,7 @@
 <div x-data="{ max: {{ $max }}, length: {{ mb_strlen($value) }} }"
     class="{{ $class }} flex py-1.5 px-3 block mt-1 w-full rounded-md shadow-sm shadow-logo-color text-gray-950 bg-white dark:bg-zinc-950 dark:text-gray-200 border-0 ring-1 ring-inset ring-gray-300 dark:ring-zinc-700 focus-within:ring-indigo-500 dark:focus-within:ring-indigo-500">
     <textarea name="{{ $name }}" {{ $disabled ? 'disabled' : '' }} value="{{ $value }}" {{ $attributes }}
-        class="w-full resize-none overflow-hidden bg-transparent border-0 p-0 focus:outline-none focus:ring-0"
+        class="w-full min-h-6 resize-none overflow-hidden bg-transparent border-0 p-0 focus:outline-none focus:ring-0"
         x-data="{
             resize() {
                 $el.style.height = '0px';
