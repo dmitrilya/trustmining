2090504787031add1da359ce45e1cd4f3e8bc04f
dmitrilya <dmitrievilyan@gmail.com>
2026-03-03 18:07:15 +0800

    fix

--- resources/views/ad/show.blade.php
+++ resources/views/ad/show.blade.php
@@ -682,7 +682,7 @@
             </div>
         </div>
 
-        @if ($ad->adCategory->name == 'miners')
+        @if ($ad->adCategory->name == 'miners' && isset($ads))
             <section class="mt-4 sm:mt-6 lg:mt-8">
                 <div class="flex items-center justify-between px-4 py-1.5 lg:px-5 lg:py-2 gap-4 mb-2 sm:mb-3">
                     <h2 class="font-bold text-xl sm:text-2xl text-slate-900 dark:text-slate-100">
