b93bc91fb774712e4c1121dd6f05089baac80218
dmitrilya <dmitrievilyan@gmail.com>
2026-04-10 16:58:59 +0800

    calculator optimization

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
+use Illuminate\View\View as ViewBlade;
 
 use App\Models\Database\AsicModel;
 use App\Models\Database\AsicVersion;
 use App\Models\Database\Coin;
+use App\Models\Morph\View;
 
 class CalculatorController extends Controller
 {
-    public function calculator(Request $request, ?AsicModel $asicModel = null, ?AsicVersion $asicVersion = null): View
+    public function calculator(Request $request, ?AsicModel $asicModel = null, ?AsicVersion $asicVersion = null): ViewBlade
     {
         $models = Cache::get('calculator_models');
 
@@ -23,27 +25,31 @@ class CalculatorController extends Controller
         $ads = $this->getAds()->where('asic_models.id', $selModel->id)
             ->orderByRaw('ads.price = 0')->orderByRaw("ads.price * coin_rates.rate")->limit(9)->get();
 
-        return view('calculator.index', [
-            'models' => $models->map(fn($model) => [
-                'id' => $model->id,
-                'name' => $model->name,
-                'asic_versions' => $model->asicVersions->map(function ($version) {
-                    $versionData = $version->toArray();
-                    $versionData['ads_count'] = count($version->ads);
-                    unset($versionData['ads']);
-                    unset($versionData['price_data']);
-                    unset($versionData['asic_model_id']);
-                    unset($versionData['original_hashrate']);
-                    unset($versionData['original_efficiency']);
-                    unset($versionData['brand_name']);
-                    $versionData['profits'] = $versionData['profits']->map(fn($profit) => [
-                        'profit' => $profit['profit'],
-                        'coins' => $profit['coins']->map(fn($coin) => $coin->only(['name', 'abbreviation', 'profit']))->toArray()
-                    ])->toArray();
+        $models = $models->map(fn($model) => [
+            'id' => $model->id,
+            'name' => $model->name,
+            'asic_versions' => $model->asicVersions->map(function ($version) {
+                $versionData = $version->toArray();
+                $versionData['ads_count'] = count($version->ads);
+                unset($versionData['ads']);
+                unset($versionData['price_data']);
+                unset($versionData['asic_model_id']);
+                unset($versionData['original_hashrate']);
+                unset($versionData['original_efficiency']);
+                unset($versionData['brand_name']);
+                $versionData['profits'] = $versionData['profits']->map(fn($profit) => [
+                    'profit' => $profit['profit'],
+                    'coins' => $profit['coins']->map(fn($coin) => $coin->only(['name', 'abbreviation', 'profit']))->toArray()
+                ])->toArray();
 
-                    return $versionData;
-                })->toArray()
-            ]),
+                return $versionData;
+            })->toArray()
+        ]);
+
+        return view('calculator.index', [
+            'models' => $models,
+            'popularModels' => $models->whereIn('id', View::where('viewable_type', 'asic-model')->select('viewable_id', DB::raw('count(*) as views_count'))
+                ->groupBy('viewable_id')->orderBy('views_count', 'desc')->limit(30)->pluck('viewable_id')),
             'rub' => Coin::where('abbreviation', 'RUB')->first('id')->rate,
             'rModel' => $asicModel,
             'rVersion' => $asicVersion,
@@ -53,7 +59,7 @@ class CalculatorController extends Controller
         ]);
     }
 
-    public function calculatorApp(Request $request, ?AsicModel $asicModel = null, ?AsicVersion $asicVersion = null): View
+    public function calculatorApp(Request $request, ?AsicModel $asicModel = null, ?AsicVersion $asicVersion = null): ViewBlade
     {
         $models = Cache::get('calculator_models');
 
@@ -89,7 +95,7 @@ class CalculatorController extends Controller
         ]);
     }
 
-    public function calculatorWidjet(Request $request): View
+    public function calculatorWidjet(Request $request): ViewBlade
     {
         $models = Cache::get('calculator_models');
 
--- resources/views/calculator/components/selectversion.blade.php
+++ resources/views/calculator/components/selectversion.blade.php
@@ -1,85 +1,68 @@
 <div style="height: 118.67px" x-data="{
+    init: false,
     models: {{ $models->values() }},
+    popular_models: {{ $popularModels->values() }},
     selectedModel: {{ $selectedModel }},
     selectedVersion: {{ $selectedVersion }},
     search: '{{ $selectedModel->name }}',
+    openModel: false,
+    openVersion: false,
     get filteredModels() {
-        return !this.search ? this.models : this.models.filter(m => m.name.toLowerCase().includes(this.search.toLowerCase()));
+        if (!this.search) return this.popular_models;
+
+        const s = this.search.toLowerCase();
+        return this.models.filter(m => m.name.toLowerCase().includes(s));
     },
-}">
-    <div class="relative mt-1" x-data="{ open: false }" @click.away="open = false">
-        <div class="relative z-0 w-full" @click="open = true">
-            <div class="flex items-center justify-between group border-b-2 border-slate-300 dark:border-slate-700">
-                <input type="text" autocomplete="off" :value="search" id="search_model"
-                    @input="search = $el.value;selectedModel = null;selectedVersion = null;version = null"
-                    class="block py-2.5 px-0 w-full text-sm xs:text-base text-slate-950 bg-transparent border-0 appearance-none dark:text-white group-focus:outline-none focus:ring-0 peer" />
+    selectModel(m) {
+        this.selectedModel = m;
+        this.search = m.name;
+        this.openModel = false;
+    },
+    selectVersion(v) {
+        selectedVersion = v;
+        version = v;
+        openVersion = false;
+    }
+}" x-init="$nextTick(() => init = true)">
+    <template x-if="init">
+        <div>
+            <div class="relative mt-1" @click.away="openModel = false">
+                <div class="relative z-0 w-full" @click="openModel = true">
+                    <div
+                        class="flex items-center justify-between group border-b-2 border-slate-300 dark:border-slate-700">
+                        <input type="text" autocomplete="off" :value="search" id="search_model"
+                            @input.debounce.300ms="search = $el.value;selectedModel = null;selectedVersion = null;version = null"
+                            class="block py-2.5 px-0 w-full text-sm xs:text-base text-slate-950 bg-transparent border-0 appearance-none dark:text-white group-focus:outline-none focus:ring-0 peer" />
 
-                <button type="button" aria-label="Clear"
-                    class="ml-4 flex h-4 w-4 items-center justify-center rounded-md text-slate-500 dark:text-slate-400"
-                    @click="search = '';selectedModel = null;selectedVersion = null;version = null;$el.previousElementSibling.focus()">
-                    <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                        aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+                        <button type="button" aria-label="Clear"
+                            class="ml-4 flex h-4 w-4 items-center justify-center rounded-md text-slate-500 dark:text-slate-400"
+                            @click.debounce.10ms="search = '';selectedModel = null;selectedVersion = null;version = null;$el.previousElementSibling.focus()">
+                            <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
 
-                <label for="search_model"
-                    class="absolute text-sm text-slate-600 dark:text-slate-300 duration-300 transform scale-75 -translate-y-6 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-indigo-600 peer-focus:dark:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                    {{ __('Model') }}
-                </label>
-            </div>
-        </div>
-
-        <ul role="listbox" x-show="open"
-            class="overflow-y-auto absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white dark:bg-slate-900 py-1 text-base shadow-lg shadow-logo-color ring-1 ring-black dark:ring-slate-900 ring-opacity-5 focus:outline-none sm:text-sm">
-            <template x-for="asicModel in filteredModels" :key="asicModel.id">
-                <li @click="selectedModel = asicModel; open = false; search = asicModel.name"
-                    class="relative cursor-default select-none py-2 pl-3 pr-9 text-slate-950 dark:text-slate-50 hover:bg-indigo-600 hover:text-white"
-                    role="option">
-                    <div class="flex items-center">
-                        <span class="ml-3 block truncate" x-text="asicModel.name"></span>
+                        <label for="search_model"
+                            class="absolute text-sm text-slate-600 dark:text-slate-300 duration-300 transform scale-75 -translate-y-6 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-indigo-600 peer-focus:dark:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                            {{ __('Model') }}
+                        </label>
                     </div>
+                </div>
 
-                    <span x-show="selectedModel && selectedModel.id == asicModel.id"
-                        class="absolute inset-y-0 right-0 flex items-center pr-4">
-                        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"
-                            class="text-indigo-600 hover:text-white" aria-hidden="true">
-                            <path fill-rule="evenodd" clip-rule="evenodd"
-                                d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" />
-                        </svg>
-                    </span>
-                </li>
-            </template>
-        </ul>
-    </div>
+                <ul role="listbox" x-show="openModel"
+                    class="overflow-y-auto absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white dark:bg-slate-900 py-1 text-base text-slate-950 dark:text-slate-50 shadow-lg shadow-logo-color ring-1 ring-black dark:ring-slate-900 ring-opacity-5 focus:outline-none sm:text-sm">
+                    <template x-for="asicModel in filteredModels" :key="asicModel.id">
+                        <li @click.debounce.10ms="selectModel(asicModel)" role="option"
+                            class="relative cursor-default select-none py-2 pl-3 pr-9 hover:bg-indigo-600 hover:text-white">
+                            <div class="flex items-center">
+                                <span class="ml-3 block truncate" x-text="asicModel.name"></span>
+                            </div>
 
-    <template x-if="selectedModel">
-        <div class="mt-4">
-            <p class="block text-sm text-slate-800 dark:text-slate-300">{{ __('Version') }}</p>
-
-            <div class="relative mt-1" x-data="{ show: false }" @click.away="show = false">
-                <button type="button" @click="show = !show"
-                    class="h-9 w-full cursor-pointer rounded-md text-slate-950 dark:text-slate-50 bg-white dark:bg-slate-900 py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-slate-300 dark:ring-slate-700">
-                    <span class="block truncate" x-text="selectedVersion?.hashrate ?? ''"></span>
-
-                    <span class="absolute inset-y-0 right-0 flex items-center pr-2">
-                        <svg class="h-5 w-5 text-slate-500 dark:text-slate-400" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" clip-rule="evenodd"
-                                d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
-                        </svg>
-                    </span>
-                </button>
-
-                <ul class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white dark:bg-slate-900 py-1 text-base shadow-lg"
-                    x-show="show">
-                    <template x-for="asicVersion in selectedModel.asic_versions" :key="asicVersion.id">
-                        <li @click="selectedVersion = asicVersion; version = asicVersion; show = false;"
-                            class="cursor-default relative select-none py-2 pl-3 pr-9 text-slate-950 dark:text-slate-50 hover:bg-indigo-600 hover:text-white">
-                            <span class="block truncate" x-text="asicVersion.hashrate"></span>
-
-                            <span x-show="selectedVersion?.id == asicVersion.id"
+                            <span x-show="selectedModel && selectedModel.id == asicModel.id"
                                 class="absolute inset-y-0 right-0 flex items-center pr-4">
-                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"
+                                    class="text-indigo-600 hover:text-white" aria-hidden="true">
                                     <path fill-rule="evenodd" clip-rule="evenodd"
                                         d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" />
                                 </svg>
@@ -88,6 +71,45 @@
                     </template>
                 </ul>
             </div>
+
+            <template x-if="selectedModel">
+                <div class="mt-4">
+                    <p class="block text-sm text-slate-800 dark:text-slate-300">{{ __('Version') }}</p>
+
+                    <div class="relative mt-1" @click.away="openVersion = false">
+                        <button type="button" @click="openVersion = !openVersion"
+                            class="h-9 w-full cursor-pointer rounded-md text-slate-950 dark:text-slate-50 bg-white dark:bg-slate-900 py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-slate-300 dark:ring-slate-700">
+                            <span class="block truncate" x-text="selectedVersion?.hashrate ?? ''"></span>
+
+                            <span class="absolute inset-y-0 right-0 flex items-center pr-2">
+                                <svg class="h-5 w-5 text-slate-500 dark:text-slate-400" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
+                                </svg>
+                            </span>
+                        </button>
+
+                        <ul class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white dark:bg-slate-900 py-1 text-base text-slate-950 dark:text-slate-50 shadow-lg"
+                            x-show="openVersion">
+                            <template x-for="asicVersion in selectedModel.asic_versions" :key="asicVersion.id">
+                                <li @click.debounce.10ms="selectVersion(asicVersion)"
+                                    class="cursor-default relative select-none py-2 pl-3 pr-9 hover:bg-indigo-600 hover:text-white">
+                                    <span class="block truncate" x-text="asicVersion.hashrate"></span>
+
+                                    <span x-show="selectedVersion?.id == asicVersion.id"
+                                        class="absolute inset-y-0 right-0 flex items-center pr-4">
+                                        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                                d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" />
+                                        </svg>
+                                    </span>
+                                </li>
+                            </template>
+                        </ul>
+                    </div>
+                </div>
+            </template>
         </div>
     </template>
 </div>
