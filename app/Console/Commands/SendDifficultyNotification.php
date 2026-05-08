9c7dfb39205a817bdb2597537554a75a09e4f6b7
dmitrilya <dmitrievilyan@gmail.com>
2026-05-08 12:02:13 +0800

    fix

--- app/Console/Commands/SendDifficultyNotification.php
+++ app/Console/Commands/SendDifficultyNotification.php
@@ -57,7 +57,7 @@ class SendDifficultyNotification extends Command
         foreach ($coins as $coin) {
             $text = "🔔 *{$coin['name']} difficulty alert*\n\n";
             $text .= __('Current difficulty') . ': ' . $coin['difficultyData']['needBlocksTime'] . "\n";
-            $text .= __('Blocks before recalculation') . ': ' . $coin['difficultyData']['lastDifficulty'] . "\n";
+            $text .= __('Blocks before recalculation') . ': ' . $coin['difficultyData']['lastDifficulty']['difficulty'] . "\n";
             $text .= __('Next difficulty prediction') . ': ' . ($coin['difficultyData']['prediction'] > 0 ?? '+') . $coin['difficultyData']['prediction'] . '%';
 
             SendTGNotifications::dispatch($coin['tgIds'], 'Difficulty alert', null, null, ['coin' => $coin['name'], 'text' => $text]);
