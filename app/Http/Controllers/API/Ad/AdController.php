acd5c6c0012f77aab4941ac1ea6783fbd5a696be
dmitrilya <dmitrievilyan@gmail.com>
2026-05-14 17:47:45 +0800

    fix

--- app/Http/Controllers/API/Ad/AdController.php
+++ app/Http/Controllers/API/Ad/AdController.php
@@ -25,26 +25,27 @@ class AdController extends Controller
      */
     public function get(Request $request)
     {
-        $ads = $this->getAds($request, AdCategory::where('name', 'miners')->first())->get()->map(function ($ad) {
-            $ad->props = json_decode($ad->props, true);
-            $props = [
-                'condition' => $ad->props['Condition'] == 'Used' ? 'used' : 'new',
-                'availability' => $ad->props['Availability'] == 'Preorder' ? 'preorder' : 'in_stock',
-            ];
-            
-            if ($ad->props['Condition'] == 'Used') $props['warranty'] = $ad->props['Warranty (months)'];
-            if ($ad->props['Availability'] == 'Preorder') $props['waiting'] = $ad->props['Waiting (days)'];
+        $ads = $this->getAds($request, AdCategory::where('name', 'miners')->first())->where('user_id', $request->user()->id)
+            ->get()->map(function ($ad) {
+                $ad->props = json_decode($ad->props, true);
+                $props = [
+                    'condition' => $ad->props['Condition'] == 'Used' ? 'used' : 'new',
+                    'availability' => $ad->props['Availability'] == 'Preorder' ? 'preorder' : 'in_stock',
+                ];
 
-            return [
-                'id' => $ad->id,
-                'name' => $ad->asic_brand_name . ' ' . $ad->asic_model_name . ' ' . $ad->asic_version_hashrate . $ad->asic_version_measurement,
-                'props' => $props,
-                'price' => $ad->price,
-                'coin' => strtolower($ad->coin),
-                'with_vat' => $ad->with_vat,
-                'hidden' => $ad->hidden,
-            ];
-        });
+                if ($ad->props['Condition'] == 'Used') $props['warranty'] = $ad->props['Warranty (months)'];
+                if ($ad->props['Availability'] == 'Preorder') $props['waiting'] = $ad->props['Waiting (days)'];
+
+                return [
+                    'id' => $ad->id,
+                    'name' => $ad->asic_brand_name . ' ' . $ad->asic_model_name . ' ' . $ad->asic_version_hashrate . $ad->asic_version_measurement,
+                    'props' => $props,
+                    'price' => $ad->price,
+                    'coin' => strtolower($ad->coin),
+                    'with_vat' => $ad->with_vat,
+                    'hidden' => $ad->hidden,
+                ];
+            });
 
         return response()->json([
             'ads' => $ads
