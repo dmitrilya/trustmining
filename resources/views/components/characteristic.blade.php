0878dfc7a75a21b3d02e2474b17c698ea6259dd1
dmitrilya <dmitrievilyan@gmail.com>
2026-05-16 00:41:38 +0800

    fix

--- resources/views/components/characteristic.blade.php
+++ resources/views/components/characteristic.blade.php
@@ -1,12 +1,12 @@
 @props(['name', 'value' => null, 'xValue' => null, 'itemprop' => null, 'unit' => null])
 
 <li @if ($itemprop) itemprop="{{ $itemprop }}" itemscope itemtype="{{ $itemprop == 'additionalProperty' ? 'http://schema.org/PropertyValue' : 'http://schema.org/QuantitativeValue' }}" @endif
-    class="flex justify-between items-end text-xs sm:text-sm text-slate-500 dark:text-slate-400">
+    class="flex justify-between items-end text-sm text-slate-600 dark:text-slate-400">
     <span
         @if ($itemprop) itemprop="{{ $itemprop == 'additionalProperty' ? 'name' : 'valueReference' }}" @endif>{{ __($name) }}</span>
     <span class="dots mx-1 xs:mx-2 win-w-[1.25rem]"></span>
     @if (!is_array($value))
-        <span @if ($itemprop) itemprop="value" @endif class="text-slate-700 dark:text-slate-300 text-right truncate"
+        <span @if ($itemprop) itemprop="value" @endif class="text-slate-800 dark:text-slate-200 text-right truncate"
             @if ($xValue) x-text="{{ $xValue }}" @endif>{{ __($value) }}</span>
     @else
         <div class="flex flex-wrap gap-0.5 sm:gap-1 mt-2">
--- resources/views/database/asic-miners/model.blade.php
+++ resources/views/database/asic-miners/model.blade.php
@@ -44,17 +44,17 @@
 
                     <div class="md:col-span-2 md:col-start-1 mt-4 md:mt-8" x-data="{ selectedTab: {{ array_search($selectedVersion->id, $versions->pluck('id')->toArray()) }} }">
                         <div
-                            class="text-xs text-center text-slate-600 border-b border-slate-300 dark:text-slate-300 dark:border-slate-800">
+                            class="text-xs font-semibold text-center text-slate-600 dark:text-slate-300 border-b-2 border-slate-300 dark:border-slate-700">
                             <ul class="flex -mb-px overflow-x-auto no-scrollbar whitespace-nowrap [mask-image:linear-gradient(to_right,black_80%,transparent_100%)]">
                                 @foreach ($versions as $i => $version)
-                                    <li class="mr-1 sm:mr-2">
+                                    <li class="mr-1 sm:mr-2 -mb-[1px]">
                                         <button
                                             class="inline-block p-1 xs:p-2 border-b-2 rounded-t-lg"
                                             @click="selectedTab = {{ $i }}"
                                             :class="{
-                                                'border-transparent hover:text-slate-600 hover:border-slate-300 dark:hover:text-slate-300': {{ $i }} !=
+                                                'border-transparent hover:text-slate-600 dark:hover:text-slate-300 hover:border-slate-400 dark:hover:border-slate-600': {{ $i }} !=
                                                     selectedTab,
-                                                'text-indigo-500 border-indigo-600 active dark:text-indigo-500 dark:border-indigo-600': {{ $i }} ==
+                                                'text-indigo-500 border-indigo-600 active dark:text-indigo-500 dark:border-indigo-500': {{ $i }} ==
                                                     selectedTab
                                             }">
                                             {{ $version->hashrate }}{{ $version->measurement }}
@@ -178,7 +178,7 @@
                                                 <div class="text-xs sm:text-sm text-slate-600 dark:text-slate-500">
                                                     {{ $coin->abbreviation }}
                                                 </div>
-                                                <div class="text-xxs sm:text-xs text-slate-400 dark:text-slate-300">
+                                                <div class="text-xxs xs:text-xs text-slate-400 dark:text-slate-300">
                                                     {{ $coin->name }}
                                                 </div>
                                                 <div
