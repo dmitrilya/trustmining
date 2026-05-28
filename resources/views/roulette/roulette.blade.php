f10eb2717ff0f3bdde46e7b3e134809e34d2778f
dmitrilya <dmitrievilyan@gmail.com>
2026-05-28 11:30:55 +0800

    roulette fix

--- resources/views/roulette/roulette.blade.php
+++ resources/views/roulette/roulette.blade.php
@@ -1,13 +1,13 @@
 <div x-data="roulette({{ $roulettePrizes }}, {{ $timeToSpin }})">
     <x-modal name="roulette" maxWidth="sm" rounded="rounded-xl">
-        <div class="p-6 flex flex-col items-center" x-init="$watch('isSpinning', value => { if (!value && wonPrize) $dispatch('close'); })">
+        <div class="p-2 xs:p-4 sm:p-6 flex flex-col items-center" x-init="$watch('isSpinning', value => { if (!value && wonPrize) $dispatch('close'); })">
             <h2 class="text-2xl font-black text-slate-800 dark:text-slate-200 mb-2 tracking-tight">TM Roulette</h2>
             <p class="text-center text-xs sm:text-sm text-slate-600 dark:text-slate-400 mb-8">
                 {{ __('Win VIP tools and prizes from partners') }}
                 {{ trans_choice('time.period_choice', config('settings.roulette.period'), ['count' => config('settings.roulette.period')]) }}
             </p>
 
-            <div class="relative w-80 h-80 mx-auto mb-8 flex items-center justify-center">
+            <div class="relative w-64 xs:w-80 h-64 xs:h-80 mx-auto mb-8 flex items-center justify-center">
                 <div
                     class="absolute -top-3 left-1/2 -translate-x-1/2 z-30 w-0 h-0 border-l-[15px] border-l-transparent border-r-[15px] border-r-transparent border-t-[25px] border-t-slate-300 dark:border-t-slate-700 drop-shadow-[0_2px_5px_rgba(0,0,0,0.5)]">
                 </div>
@@ -20,15 +20,15 @@
                             :style="`transform: rotate(${(index * sectorAngle) + (sectorAngle / 2)}deg)`">
 
                             <div
-                                class="absolute top-5 w-28 h-[88px] flex flex-col items-center justify-between origin-center">
+                                class="absolute top-4 xs:top-5 w-[88px] xs:w-28 h-[74px] xs:h-[88px] flex flex-col items-center justify-between origin-center">
                                 <span class="font-black uppercase tracking-wider select-none text-center max-w-full"
                                     :class="[
                                         index % 2 === 0 ? 'text-slate-800' : 'text-slate-200',
-                                        prize.name.length > 16 ? 'text-xs' : 'text-sm'
+                                        prize.name.length > 16 ? 'text-xxs xs:text-xs' : 'text-xs xs:text-sm'
                                     ]"
                                     x-text="prize.name">
                                 </span>
-                                <a class="size-12 rounded-full overflow-hidden hover:ring ring-indigo-600 pointer-events-auto z-20"
+                                <a class="size-9 xs:size-12 rounded-full overflow-hidden hover:ring ring-indigo-600 pointer-events-auto z-20"
                                     :href="prize.partner_link" target="_blank"
                                     :aria-label="`${prize.name} partner link`">
                                     <img class="w-full" :src="prize?.id != 3 ? `/storage/${prize.user.company.logo}` : '/img/hf_logo.webp'" :alt="`${prize.name} icon`">
