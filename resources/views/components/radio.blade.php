ba785502e1ff4aeecccc06dbad2ed7691496fc00
dmitrilya <dmitrievilyan@gmail.com>
2026-06-04 01:04:48 +0800

    redio fix

--- resources/views/components/radio.blade.php
+++ resources/views/components/radio.blade.php
@@ -1,7 +1,5 @@
 @props([
     'class' => '',
-    'disabled' => false,
-    'name',
     'textClasses' => 'text-slate-800 dark:text-slate-200',
     'handleChange' => null,
     'sm' => null,
@@ -9,9 +7,8 @@
 
 <div class="{{ $class }}">
     <label class="flex items-center cursor-pointer group">
-        <input @if ($disabled) disabled @endif
-            @if ($handleChange) @change="{{ $handleChange }}($el.value)" @endif type="radio"
-            name="{{ $name }}" {{ $attributes->merge(['class' => 'sr-only peer']) }}>
+        <input @if ($handleChange) @change="{{ $handleChange }}($el.value)" @endif type="radio"
+            {{ $attributes->merge(['class' => 'sr-only peer']) }}>
 
         <div
             class="{{ $sm ? 'mr-1 sm:mr-2 w-3 h-3 sm:w-4 sm:h-4' : 'mr-2 w-4 h-4' }} flex items-center justify-center shrink-0 bg-slate-100 dark:bg-slate-900 peer-checked:bg-indigo-600 border border-slate-300 dark:border-slate-700 rounded-full transition-all duration-100 text-transparent peer-checked:text-white peer-disabled:opacity-50 peer-disabled:cursor-not-allowed">
