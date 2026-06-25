b4699b575539e861f41dc8d1fd7a3f91a4ed8ab8
dmitrilya <dmitrievilyan@gmail.com>
2026-06-25 18:59:35 +0800

    fix

--- resources/views/database/asic-miners/compare-carousel.blade.php
+++ resources/views/database/asic-miners/compare-carousel.blade.php
@@ -6,7 +6,7 @@
 
         @foreach ($asicModels as $asicModel)
             <div itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" draggable="false"
-                class="shrink-0 snap-start mr-2 sm:mr-4 w-[calc(100%-1.4rem)] xs:w-[calc(50%-1rem)] sm:w-[calc(50%-1.6rem)] md:w-[calc(33.333%-1.5rem)] lg:w-[calc(50%-1.7rem)] xl:w-[calc(33.333%-1.5rem)]">
+                class="shrink-0 snap-start mr-2 sm:mr-4 w-[calc(100%-1.4rem)] xs:w-[calc(50%-1rem)] sm:w-[calc(50%-1.6rem)] md:w-[calc(33.333%-1.5rem)] xl:w-[calc(25%-1.5rem)]">
                 <meta itemprop="position" content="{{ $loop->iteration }}" />
 
                 @include('database.asic-miners.card', [
