bbad2336975a8e3b5aac1940bc307fffada753de
dmitrilya <dmitrievilyan@gmail.com>
2026-07-06 16:49:22 +0800

    fix

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block text-sm text-slate-800 dark:text-slate-300']) }}>
+<label {{ $attributes->merge(['class' => 'block text-sm text-slate-600 dark:text-slate-400']) }}>
     {{ $value ?? $slot }}
 </label>
