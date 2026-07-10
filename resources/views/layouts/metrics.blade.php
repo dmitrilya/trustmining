e0ac478f39421ecdb41d41f5780e0f09f205aed4
dmitrilya <dmitrievilyan@gmail.com>
2026-07-10 11:47:24 +0800

    fix

--- resources/views/layouts/metrics.blade.php
+++ resources/views/layouts/metrics.blade.php
@@ -7,12 +7,11 @@
 
     <div class="max-w-9xl mx-auto p-2 sm:p-6 lg:p-8" x-data="{ show: false }">
         <div class="lg:grid grid-cols-9 gap-4 items-start relative">
-            <div :class="{ '-left-full': !show, 'left-2 sm:left-4': show }"
-                class="absolute top-16 sm:top-22 duration-300 lg:top-0 bg-slate-100 lg:left-0 lg:relative lg:col-span-3 xl:col-span-2 lg:bg-white/40 dark:bg-slate-900/40 backdrop-blur-xl border border-slate-300 dark:border-slate-700 overflow-hidden shadow-sm shadow-logo-color rounded-xl p-3 sm:p-4 lg:p-6">
+            <grid class="grid-cols-1 gap-4">
                 @include('layouts.components.metrics-menu')
                 <x-ai-kodex targetWidth="1024" />
-            </div>
-
+            </grid>
+                
             <div class="lg:col-span-6 xl:col-span-7">
                 {{ $slot }}
             </div>
