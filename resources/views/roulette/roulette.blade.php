65a41dedf7b66ca32fb0e9ad71566adccb43cdd2
dmitrilya <dmitrievilyan@gmail.com>
2026-07-19 13:26:19 +0800

    fix

--- resources/views/roulette/roulette.blade.php
+++ resources/views/roulette/roulette.blade.php
@@ -59,7 +59,7 @@
                                 <div class="w-10 h-10 rounded-full overflow-hidden border border-slate-300 dark:border-slate-700 relative"
                                     :class="getPrizeRarityClasses(prize.chance).glow">
                                     <img class="w-full h-full object-contain"
-                                        :src="prize?.id != 3 ? `/storage/${prize.user.company.logo}` : '/img/hf_logo.webp'" :alt="`${prize.name} icon`">
+                                        :src="`/storage/${prize.user.company.logo}`" :alt="`${prize.name} icon`">
                                 </div>
                             </div>
                         </template>
@@ -73,13 +73,13 @@
                 </div>
 
                 <div class="grid grid-cols-3 xs:grid-cols-4 md:grid-cols-6 justify-items-center gap-3 xs:gap-4">
-                    <template x-for="prize in prizes.filter(p => p.id !== 3)" :key="prize.id">
+                    <template x-for="prize in prizes" :key="prize.id">
                         <a class="flex flex-col items-center relative group" :href="prize.partner_link" target="_blank" :aria-label="`${prize.name} partner link`">
                             <img class="w-12 h-12 xs:w-16 xs:h-16 rounded-full group-hover:ring ring-indigo-600 pointer-events-auto"
                                 :src="`/storage/${prize.user.company.logo}`" :alt="`${prize.name} icon`">
                             <div class="text-center mt-1 xs:mt-2 text-xs sm:text-sm text-slate-800 dark:text-slate-200" x-text="prize.user.name"></div>
                             <div
-                                class="pointer-events-none absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block bg-slate-950 text-white text-[9px] font-bold py-1 px-2 rounded shadow-md whitespace-nowrap z-50">
+                                class="pointer-events-none absolute bottom-full left-1/2 -translate-x-1/2 mb-1 hidden group-hover:block bg-slate-950 text-white text-xxs font-bold py-1 px-2 rounded shadow-md whitespace-nowrap z-50">
                                 <span x-text="prize.name"></span>
                             </div>
                         </a>
@@ -108,7 +108,7 @@
             </h3>
             <a class="w-20 h-20 rounded-full overflow-hidden hover:ring ring-indigo-600 mb-3" :href="wonPrize?.partner_link">
                 <template x-if="wonPrize">
-                    <img class="w-full" :src="wonPrize.id != 3 ? `/storage/${wonPrize.user.company.logo}` : '/img/hf_logo.webp'"
+                    <img class="w-full" :src="`/storage/${wonPrize.user.company.logo}`"
                         :alt="`${wonPrize.name} icon`">
                 </template>
             </a>
