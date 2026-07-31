a4fe4f6d3562992a715d1c9b7ab72affee94f5f5
dmitrilya <dmitrievilyan@gmail.com>
2026-07-31 11:59:47 +0800

    fix

--- resources/views/ad/firmwares/edit.blade.php
+++ resources/views/ad/firmwares/edit.blade.php
@@ -36,8 +36,21 @@ quill.root.innerHTML = `{{ $ad->description }}`;
 quill.on('text-change', () => description = quill.root.innerHTML);">
     <input type="hidden" name="props" x-ref="props_firmwares" value="{{ json_encode($ad->props) }}">
 
-    <x-inputs.multiselect name="models" label="For which models" :all="App\Models\Database\AsicModel::pluck('name')->diff($ad->props['For which models'])->values()" :selected="collect($ad->props['For which models'])"
-        handleChange="let props = JSON.parse($refs.props_firmwares.value); props['For which models'] = selected; $refs.props_firmwares.value = JSON.stringify(props);" />
+    <div class="relative z-0 w-full group">
+        <input type="text" id="asic_model" disabled value="{{ $ad->asicVersion->asicModel->name }}"
+            class="block py-2.5 px-0 w-full text-sm text-slate-800 bg-transparent border-0 border-b-2 border-slate-300 appearance-none dark:text-slate-200 dark:border-slate-700 dark:focus:border-indigo-500 focus:outline-none focus:ring-0 focus:border-indigo-500 peer" />
+        <label for="asic_model"
+            class="absolute text-sm text-slate-600 dark:text-slate-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-indigo-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+            {{ __('Model') }}
+        </label>
+    </div>
+
+    <div>
+        <x-inputs.input-label for="asic_version" :value="__('Version')" />
+        <x-inputs.text-input id="asic_version" disabled :value="$ad->asicVersion->hashrate" />
+    </div>
+
+    @include('ad.firmwares.modes', ['modes' => collect($ad->props['Modes'])])
 
     <div id="editor-wrap" class="bg-slate-100 dark:bg-slate-950 rounded-xl">
         <div id="editor"
