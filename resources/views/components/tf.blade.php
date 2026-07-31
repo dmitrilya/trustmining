bf8934f868f9be5db19504860af441d934a43a3f
dmitrilya <dmitrievilyan@gmail.com>
2026-07-31 18:19:25 +0800

    fix

--- resources/views/components/tf.blade.php
+++ resources/views/components/tf.blade.php
@@ -1,7 +1,7 @@
 @props(['tf'])
 
 <div
-    {{ $attributes->merge(['class' => 'flex items-center w-fit px-1.5 py-0.5 rounded-full border ' . ($tf > config('trustfactor.yellow') ? ($tf > config('trustfactor.green') ? 'bg-emerald-500/10 border-emerald-500/30 text-emerald-800 dark:text-emerald-400' : 'bg-amber-500/10 border-amber-500/30 text-amber-800 dark:text-amber-200') : 'bg-rose-500/10 border-rose-500/30 text-rose-600 dark:text-rose-400')]) }}>
+    {{ $attributes->merge(['class' => 'flex items-center w-fit px-1.5 py-0.5 rounded-full border ' . ($tf > config('trustfactor.yellow') ? ($tf > config('trustfactor.green') ? 'bg-emerald-500/10 border-emerald-500/30 text-emerald-800 dark:text-emerald-400' : 'bg-amber-500/10 border-amber-500/30 text-amber-600 dark:text-amber-400') : 'bg-rose-500/10 border-rose-500/30 text-rose-600 dark:text-rose-400')]) }}>
     <svg class="trust mr-1 sm:mr-2 w-2 h-2 xs:w-3 xs:h-3" width="100" height="100" viewBox="0 0 100 100" xmlns="http://w3.org">
         <circle cx="50" cy="50" r="40" fill="currentColor" />
     </svg>
