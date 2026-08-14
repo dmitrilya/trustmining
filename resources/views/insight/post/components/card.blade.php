fb12a9f3057dacd35ff5affe177f4fc4ab3316a6
dmitrilya <dmitrievilyan@gmail.com>
2026-08-14 14:13:35 +0800

    hide views and likes from insight cards

--- resources/views/insight/post/components/card.blade.php
+++ resources/views/insight/post/components/card.blade.php
@@ -50,12 +50,12 @@
                 <div class="flex items-center gap-2 xs:gap-3 sm:gap-4">
                     <div itemprop="interactionStatistic" itemscope itemtype="https://schema.org/InteractionCounter"
                         class="flex items-center">
-                        <svg class="w-3 h-3 xs:w-4 xs:h-4 text-slate-800 dark:text-slate-200" aria-hidden="true"
+                        {{-- <svg class="w-3 h-3 xs:w-4 xs:h-4 text-slate-800 dark:text-slate-200" aria-hidden="true"
                             height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd"
                                 d="M15.03 9.684h3.965c.322 0 .64.08.925.232.286.153.532.374.717.645a2.109 2.109 0 0 1 .242 1.883l-2.36 7.201c-.288.814-.48 1.355-1.884 1.355-2.072 0-4.276-.677-6.157-1.256-.472-.145-.924-.284-1.348-.404h-.115V9.478a25.485 25.485 0 0 0 4.238-5.514 1.8 1.8 0 0 1 .901-.83 1.74 1.74 0 0 1 1.21-.048c.396.13.736.397.96.757.225.36.32.788.269 1.211l-1.562 4.63ZM4.177 10H7v8a2 2 0 1 1-4 0v-6.823C3 10.527 3.527 10 4.176 10Z"
                                 clip-rule="evenodd" />
-                        </svg>
+                        </svg> --}}
 
                         <meta itemprop="interactionType" content="https://schema.org/LikeAction" />
                         <meta itemprop="userInteractionCount" content="{{ $post->likes_count }}" />
