c1c0468e2d616882271f859328553071f41d391a
dmitrilya <dmitrievilyan@gmail.com>
2026-08-14 12:23:37 +0800

    fix

--- app/Console/Commands/UpdateCompanyCards.php
+++ app/Console/Commands/UpdateCompanyCards.php
@@ -31,7 +31,7 @@ class UpdateCompanyCards extends Command
     public function handle()
     {
         $checko = new CheckoService();
-        
+
         $total = Company::count();
 
         if ($total === 0) return Command::SUCCESS;
@@ -39,28 +39,25 @@ class UpdateCompanyCards extends Command
         $perDay = ceil($total / 7);
         $dayIndex = (int) date('w');
 
-        Company::query()->orderBy('id')->skip($dayIndex * $perDay)->take($perDay)
-            ->chunkById(100, function ($companies) use ($checko) {
-                foreach ($companies as $company) {
-                    $inn = $company->card['inn'] ?? null;
-                    $type = $company->card['type'] ?? null;
+        foreach (Company::skip($dayIndex * $perDay)->take($perDay) as $company) {
+            $inn = $company->card['inn'] ?? null;
+            $type = $company->card['type'] ?? null;
 
-                    if (!$inn || !$type) {
-                        info('[COMPANY CARD UPDATING] Missing inn or type {company_id=' . $company->id . '}');
-                        continue;
-                    }
+            if (!$inn || !$type) {
+                info('[COMPANY CARD UPDATING] Missing inn or type {company_id=' . $company->id . '}');
+                continue;
+            }
 
-                    $card = $checko->companyByInn($inn, $type);
+            $card = $checko->companyByInn($inn, $type);
 
-                    if (!$card) {
-                        info('[COMPANY CARD UPDATING] card in checko not exists {company_id=' . $company->id . '}');
-                        continue;
-                    }
+            if (!$card) {
+                info('[COMPANY CARD UPDATING] card in checko not exists {company_id=' . $company->id . '}');
+                continue;
+            }
 
-                    $company->card = $card['data'];
-                    $company->save();
-                }
-            });
+            $company->card = $card['data'];
+            $company->save();
+        }
 
         return Command::SUCCESS;
     }
