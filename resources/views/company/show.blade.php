ce90523ac7b5839a11a16c13b1d9ccae3a6e965f
dmitrilya <dmitrievilyan@gmail.com>
2026-08-24 12:35:57 +0800

    fix

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -154,10 +154,12 @@
             </div>
         @endif
 
-        <div
-            class="bg-white/40 dark:bg-slate-900/40 border border-slate-300 dark:border-slate-700 overflow-hidden shadow shadow-logo-color rounded-xl p-2 sm:p-4 md:p-6 lg:p-8 xl:p-10">
-            @include('company.components.tf')
-        </div>
+        @if (!isset($moderation))
+            <div
+                class="bg-white/40 dark:bg-slate-900/40 border border-slate-300 dark:border-slate-700 overflow-hidden shadow shadow-logo-color rounded-xl p-2 sm:p-4 md:p-6 lg:p-8 xl:p-10">
+                @include('company.components.tf')
+            </div>
+        @endif
 
         <div
             class="bg-white/40 dark:bg-slate-900/40 border border-slate-300 dark:border-slate-700 overflow-hidden shadow shadow-logo-color rounded-xl p-2 sm:p-4 md:p-6 lg:p-8 xl:p-10 mt-2 sm:mt-4">
