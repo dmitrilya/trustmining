a0c23d044572d7224b0ab42773aafc62e2c035e8
dmitrilya <dmitrievilyan@gmail.com>
2026-09-03 13:28:05 +0800

    fix

--- app/Services/TrustFactorService.php
+++ app/Services/TrustFactorService.php
@@ -327,7 +327,7 @@ class TrustFactorService
                 'response_time' => $user->art,
             ],
 
-            'ignores' => $user->ignores,
+            'ignores' => (bool) !$user->ignores,
 
             'registry' => [
                 'exists' => (bool) $company->registry,
