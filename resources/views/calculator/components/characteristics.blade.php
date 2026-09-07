0c3c04c13626ed7b66dd21880bf4b2146b00e537
dmitrilya <dmitrievilyan@gmail.com>
2026-09-07 16:55:43 +0800

    fix

--- resources/views/calculator/components/characteristics.blade.php
+++ resources/views/calculator/components/characteristics.blade.php
@@ -21,7 +21,8 @@
                 @if (!$widjet)
                     <x-characteristics.characteristic name="The best price" x-value="version?.p ? version.p + ' USDT' : '{{ __('No data') }}'" />
                 @endif
-                <x-characteristics.characteristic x-name="algorithms[version.a].p[profitNumber].c[0].a + 'USDT'" x-value="algorithms[version.a].p[profitNumber].c[0].r" />
+                <x-characteristics.characteristic x-name="(version ? algorithms[version.a].p[profitNumber].c[0].a : '') + 'USDT'"
+                    x-value="version ? algorithms[version.a].p[profitNumber].c[0].r : '-'" />
                 <x-characteristics.characteristic name="USDTRUB" :value="round(1 / $rub, 2)" />
             </x-characteristics.characteristics>
             @if (!$widjet)
