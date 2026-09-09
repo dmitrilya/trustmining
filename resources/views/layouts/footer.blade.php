227106dfa1ca1fb31a9c5640b4d077e95979ae04
dmitrilya <dmitrievilyan@gmail.com>
2026-09-09 15:22:29 +0800

    fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -179,7 +179,7 @@
 
         <div>
             <div class="text-sm text-slate-500">ИП Дмитриева Елизавета Николаевна</div>
-            <div class="text-sm text-slate-500 flex">ОГРНИП 325385000082654</div>
+            <div class="text-sm text-slate-500">ОГРНИП: <span>325385000082654</span></div>
         </div>
         <div class="text-xs text-slate-500" style ="max-width: 1312px;">© 2025 - {{ Carbon\Carbon::now()->year }}.
             {{ __('This is not a public offer.') }}</div>
