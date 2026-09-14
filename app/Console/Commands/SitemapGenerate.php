a5a86139751e09679311a078ece945b2a7c3932f
dmitrilya <dmitrievilyan@gmail.com>
2026-09-14 10:00:13 +0800

    fix

--- app/Console/Commands/SitemapGenerate.php
+++ app/Console/Commands/SitemapGenerate.php
@@ -50,7 +50,7 @@ class SitemapGenerate extends Command
     public function handle()
     {
         $out = '<?xml version="1.0" encoding="UTF-8"?>';
-        $out .= "\n" . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://w3.org">';
+        $out .= "\n" . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">';
 
         $out .= $this->addUrl('');
         $out .= $this->addUrl('hostings');
