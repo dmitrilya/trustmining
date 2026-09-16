b8aaba8f1e07b54061374155ab0aa02903317d26
dmitrilya <dmitrievilyan@gmail.com>
2026-09-17 02:23:48 +0800

    fix

--- app/Http/Controllers/DictionaryController.php
+++ app/Http/Controllers/DictionaryController.php
@@ -48,6 +48,8 @@ class DictionaryController extends Controller
 
             $data = __("dictionary.$category.terms.$term");
 
+            if (!$data) continue;
+
             $terms[$key] = [
                 'name' => $data['name'],
                 'caption' => $data['caption'],
