2417c53f8e357454794c63d859d51f0b5a59f030
dmitrilya <dmitrievilyan@gmail.com>
2026-09-24 22:54:58 +0800

    url generator fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,7 +32,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->extend('url', function (UrlGenerator $url, $app) {
-            return new class($app['router']->getRoutes(), $app['request'], $app->config->get('app.asset_url')) extends UrlGenerator {
+            $generator = new class($app['router']->getRoutes(), $app['request'], $app->config->get('app.asset_url')) extends UrlGenerator {
                 public function route($name, $parameters = [], $absolute = true)
                 {
                     $currentLocale = app()->getLocale();
@@ -45,6 +45,10 @@ class AppServiceProvider extends ServiceProvider
                     return parent::route($name, $parameters, $absolute);
                 }
             };
+
+            $generator->setKeyResolver($url->getKeyResolver());
+
+            return $generator;
         });
 
         \Carbon\Carbon::setLocale(app()->getLocale());
